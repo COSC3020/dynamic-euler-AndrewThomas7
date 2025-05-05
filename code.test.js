a231f289be9e4472d911f9abf4d90bfe54a58ef5
AndrewThomas7 <168784458+AndrewThomas7@users.noreply.github.com>
2025-05-05 13:57:04 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -2,5 +2,5 @@ const fs = require('fs');
 const assert = require('assert');
 eval(fs.readFileSync('code.js')+'');
 
-assert(factorial(10) === 3628800);
-assert(e(10) === 2.7182818011463845);
+assert(FactorialDynamic(10) === 3628800);
+assert(DynamicE(10) === 2.7182818011463845);
